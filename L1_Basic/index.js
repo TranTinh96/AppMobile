a40cce4277b59b3db5e9988d86d6dd6c318f927d
Tran Tinh <tranngoctinh96@gmail.com>
2019-10-22 14:25:28 +0700

    navigator

--- L1_Basic/index.js
+++ L1_Basic/index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import flatListBasic from "./src/flatList";
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => flatListBasic);
+AppRegistry.registerComponent(appName, () => App);
