679e78731f5e028d5882798c9585c53f4e8e9959
Tran Tinh <tranngoctinh96@gmail.com>
2019-10-26 00:57:45 +0700

    update

--- App/assets/styles/styles.screen.js
+++ App/assets/styles/styles.screen.js
@@ -5,16 +5,25 @@ const styles = StyleSheet.create({
     backgroundColor:'white',
     paddingTop: 8,
     paddingBottom: 2,
+    flex: 1,
+    width:"100%",
+    height:"100%"
   },
   background2: {
     backgroundColor: 'white',
     paddingTop: 0,
     paddingBottom: 0,
+    flex:1,
+    width:"100%",
+    height:"100%"
   },
   background3: {
     backgroundColor: '#fafafa',
     paddingTop: 0,
     paddingBottom: 0,
+    flex:1,
+    width:"100%",
+    height:"100%"
    
   },
   /*----------------Table---------------*/
@@ -63,19 +72,69 @@ const styles = StyleSheet.create({
    },
    /*----------------Home-----------------*/
    containerHome:{
-     paddingTop: 15,
-     flex:1,
+     height:130,
      width:"100%",
-     height:"100%"
    },
   //Scrooll View Horizontal
     scroollViewHome:{
-      backgroundColor:"white",
-      borderColor:"#e6e6e6",
-      //  borderWidth:1
+      justifyContent:"center"
     },
     horizontalHome:{
-      width:"100%"
+      width:"100%",
+
+    },
+    tabHome1:{
+        height:110,
+        width:240,
+        borderColor:"#e6e6e6",
+        borderWidth :1,
+        marginLeft:20,
+        justifyContent:"center",
+        borderRadius:5,
+    },
+    tabHome2:{
+        height:110,
+        borderColor:"#e6e6e6",
+        borderWidth :1,
+        width:240,
+        justifyContent:"center",
+        marginLeft:20,
+        borderRadius:5
+    },
+    tabHome3:{
+        height:110,
+        borderColor:"#e6e6e6",
+        borderWidth :1,
+        width:240,
+        justifyContent:"center",
+        marginLeft:20,
+        borderRadius:5
+    },
+    tabHome4:{
+        height:110,
+        borderColor:"#e6e6e6",
+        borderWidth :1,
+        width:240,
+        justifyContent:"center",
+        marginLeft:20,
+        marginRight:20,
+        borderRadius:5
+    },
+    //
+    contentHome :{
+      backgroundColor:"red",
+      height:80,
+      marginTop:20,
+      marginLeft:20,
+      marginRight: 20,
+    },
+    //Chartjs
+    chartHome:{
+      backgroundColor:"blue",
+      height:300,
+      marginTop:20,
+      marginLeft: 20,
+      marginRight: 20,
     }
  
 });
--- App/src/screens/homeScreen.js
+++ App/src/screens/homeScreen.js
@@ -10,17 +10,41 @@ export default class homeScreen extends Component {
     return (
       <Container style={styles.background3}>
         <Header name="Home"/>
-        <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#2DA1F2','#405DE6']} style={styles.containerHome}>
-          <View style={styles.scroollViewHome}>
-            <ScrollView horizontal = { true } showsHorizontalScrollIndicator = { false } style={styles.horizontalHome}>
-              <Text >Afghanistan</Text>
-              <Text >Afghanistan</Text>
-              <Text >Afghanistan</Text>
-              <Text >Afghanistan</Text>
-              <Text >Afghanistan</Text>
-            </ScrollView>
-          </View> 
-        </LinearGradient>
+        <ScrollView>
+          <LinearGradient
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 0}}
+            colors={['#2DA1F2', '#405DE6']}
+            style={styles.containerHome}>
+            <View style={styles.scroollViewHome}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                style={styles.horizontalHome}>
+                <View style={styles.tabHome1}></View>
+                <View style={styles.tabHome2}></View>
+                <View style={styles.tabHome3}></View>
+                <View style={styles.tabHome4}></View>
+              </ScrollView>
+            </View>
+          </LinearGradient>
+          <View style={styles.contentHome}>
+
+          </View>
+          <View style={styles.contentHome}>
+
+          </View>
+          <View style={styles.contentHome}>
+
+          </View>
+          <View style={styles.chartHome}>
+
+          </View>
+          <View style={styles.chartHome}>
+
+          </View>
+          <View style={{marginTop: 20}}></View>
+        </ScrollView>
       </Container>
     );
   }
